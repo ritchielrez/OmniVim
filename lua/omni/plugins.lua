47a217fa0fd21ead2618da27ee19a5e367bf843f
RitchielRez <52417892+ritchielrez@users.noreply.github.com>
2022-02-17 13:31:44 +0600

    Added colorscheme config

--- lua/omni/plugins.lua
+++ lua/omni/plugins.lua
@@ -37,6 +37,9 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
+  -- My favourite colorschemes
+  use "sainnhe/gruvbox-material"
+  
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

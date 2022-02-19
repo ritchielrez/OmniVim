7560890fedd52e2236a5943e43217a4e7859ee84
RitchielRez <52417892+ritchielrez@users.noreply.github.com>
2022-02-19 11:50:26 +0600

    fixed formatting

--- lua/omni/plugins.lua
+++ lua/omni/plugins.lua
@@ -57,6 +57,7 @@ return packer.startup(function(use)
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

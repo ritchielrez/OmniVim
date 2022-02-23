7db50aca1fe5872c263397b687efd529a072dd93
RitchielRez <52417892+ritchielrez@users.noreply.github.com>
2022-02-23 23:26:33 +0600

    fix(global): formatted rest of the config

--- lua/omni/lsp/settings/sumneko_lua.lua
+++ lua/omni/lsp/settings/sumneko_lua.lua
@@ -17,19 +17,18 @@
 -- }
 -- )
 --
-return ({
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.stdpath("config") .. "/lua"] = true,
-        },
-      },
-    },
-  },
+return {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+		},
+	},
 }
-)

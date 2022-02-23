ed6f7f2a7c28ae7e76f335fca8d89632d82e5e52
RitchielRez <52417892+ritchielrez@users.noreply.github.com>
2022-02-23 12:46:04 +0600

    fix(sumneko_lua): luadev plugin seems to not work
    Luadev plugin doesn't allow jumping to multiple configs

--- lua/omni/lsp/settings/sumneko_lua.lua
+++ lua/omni/lsp/settings/sumneko_lua.lua
@@ -1,4 +1,23 @@
-local luadev = require("lua-dev").setup({
+-- local luadev = require("lua-dev").setup({
+--   lspconfig = {
+--     settings = {
+--       Lua = {
+--         diagnostics = {
+--           globals = { "vim" },
+--         },
+--         workspace = {
+--           library = {
+--             [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+--             [vim.fn.stdpath("config") .. "/lua"] = true,
+--           },
+--         },
+--       },
+--     },
+--   }
+-- }
+-- )
+--
+return ({
   settings = {
     Lua = {
       diagnostics = {
@@ -14,5 +33,3 @@ local luadev = require("lua-dev").setup({
   },
 }
 )
-
-return (luadev)

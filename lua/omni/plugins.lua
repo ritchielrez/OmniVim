7f9a930eef89cfbc3cbbd504931e140da3ed2319
RitchielRez <ritchielrez@gmail.com>
2022-03-30 09:17:44 +0000

    fix(plugins): no need of cmdline cmp source

--- lua/omni/plugins.lua
+++ lua/omni/plugins.lua
@@ -60,7 +60,6 @@ return packer.startup(function(use)
 	use("hrsh7th/nvim-cmp") -- The completion plugin
 	use("hrsh7th/cmp-buffer") -- Buffer completions
 	use("hrsh7th/cmp-path") -- Path completions
-	use("hrsh7th/cmp-cmdline") -- Cmdline completions
 	use("saadparwaiz1/cmp_luasnip") -- Snippet completions
 	use("hrsh7th/cmp-nvim-lsp") -- Lsp completion
 	use("hrsh7th/cmp-nvim-lua") -- Neovim lua api

20ce750655d25ffd8375c3485ae197a61e10a996
RitchielRez <ritchielrez@gmail.com>
2022-03-31 10:08:40 +0000

    feat(dressing): proper config for dressing.nvim

--- lua/omni/lsp/dressing.lua
+++ lua/omni/lsp/dressing.lua
@@ -1,4 +1,9 @@
-require("dressing").setup({
+local dressing_status_ok, dressing = pcall(require, "dressing")
+if not dressing_status_ok then
+	return
+end
+
+dressing.setup({
 	input = {
 		-- Set to false to disable the vim.ui.input implementation
 		enabled = true,

91374ca18b1dfc90539c382258ba569527f621f8
RitchielRez <ritchielrez@gmail.com>
2022-04-12 19:47:33 +0600

    fix(options): increased guifont

--- lua/omni/options.lua
+++ lua/omni/options.lua
@@ -32,7 +32,7 @@ local options = {
 	wrap = false, -- display lines as one long line
 	scrolloff = 8, -- is one of my fav
 	sidescrolloff = 8,
-	guifont = "CaskaydiaCove NF:h17", -- the font used in graphical neovim applications
+	guifont = "CaskaydiaCove NF:h20", -- the font used in graphical neovim applications
 }
 
 vim.opt.shortmess:append("c")

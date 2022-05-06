da113d336d4a67eaad9e52d622c7af8c99264868
RitchielRez <ritchielrez@gmail.com>
2022-05-06 19:29:03 +0600

    fix(options): re-enabled global statusline

--- lua/omni/options.lua
+++ lua/omni/options.lua
@@ -8,6 +8,7 @@ local options = {
 	hlsearch = true, -- highlight all matches on previous search pattern
 	ignorecase = true, -- ignore case in search patterns
 	mouse = "a", -- allow the mouse to be used in neovim
+  laststatus = 3, -- enables single statusline for whole neovim window
 	pumheight = 10, -- pop up menu height
 	showmode = false, -- we don't need to see things like -- INSERT -- anymore
 	showtabline = 2, -- always show tabs

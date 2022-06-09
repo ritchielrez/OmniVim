e8e95e57fbe609012deaecdf27a2b410c3cc6ed5
RitchielRez <ritchielrez@gmail.com>
2022-06-09 19:15:07 +0000

    fix(colorscheme): fully transparent statusline

--- lua/omni/colorscheme.lua
+++ lua/omni/colorscheme.lua
@@ -15,6 +15,9 @@ try
   hi SignColumn guibg=none ctermbg=none
   hi EndOfBuffer guibg=none ctermbg=none
   hi NormalFloat guibg=none ctermbg=none
+  hi StatusLine guibg=none ctermbg=none
+  hi StatusLineNC guibg=none ctermbg=none
+
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
